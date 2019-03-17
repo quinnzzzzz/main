ef19babe6518b0088fba18922e251114ad5f875b
articstranger <articstranger@hotmail.com>
2019-03-18 00:29:20 +0800

    AddressBookParser: updated calling method for MapCommand MapCommandParser: added parseCriteria function

--- src/main/java/seedu/address/logic/parser/MapCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MapCommandParser.java
@@ -1,8 +1,12 @@
 package seedu.address.logic.parser;
 
+import javafx.util.Pair;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.MapCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.MapObject;
+
+import java.util.Arrays;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.*;
@@ -21,8 +25,8 @@ public class MapCommandParser implements Parser<MapCommand> {
         if (noPrefixes(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MapCommand.MESSAGE_USAGE));
         }
-
-        return new MapCommand();
+        MapObject newMap = parseCriteria(argMultimap);
+        return new MapCommand(newMap);
     }
 
     /**
@@ -35,11 +39,50 @@ public class MapCommandParser implements Parser<MapCommand> {
         return false;
     }
 
-    private boolean noPrefixes(ArgumentMultimap argMultimap){
-        if(argMultimap.getAllValues(PREFIX_YEAR).isEmpty() && argMultimap.getAllValues(PREFIX_RACE).isEmpty()
-                && argMultimap.getAllValues(PREFIX_MEDICAL).isEmpty()){
-            return true;}
-        return false;
+    /**
+     * Splits tokens into proper data for MapCommand() to handle
+     */
+    private MapObject parseCriteria(ArgumentMultimap argMultimap){
+        int prefixPoints;
+        String prefixCriteria;
+        String yearOperator = null;
+        String criteriaHolder;
+
+        Pair<Integer, Integer> localAgePair = new Pair<>(0,0);
+        Pair<Integer, String> localRacePair = new Pair<>(0,"");
+        Pair<Integer, String> localMedicalPair = new Pair<>(0,"");
+
+        //TODO: write input checks on the criteria for all
+        if(!argMultimap.getAllValues(PREFIX_YEAR).isEmpty()){
+            criteriaHolder = argMultimap.getValue(PREFIX_YEAR).get();
+            prefixPoints = Integer.parseInt(criteriaHolder.substring(0,1));
+            yearOperator = criteriaHolder.substring(1,2);
+            prefixCriteria = criteriaHolder.substring(2,criteriaHolder.length());
+            localAgePair = new Pair<>(prefixPoints,Integer.parseInt(prefixCriteria));
+        } else {
+            localAgePair = new Pair<>(0,0);
+        }
+
+        if(!argMultimap.getAllValues(PREFIX_RACE).isEmpty()){
+            criteriaHolder = argMultimap.getValue(PREFIX_RACE).get();
+            prefixPoints = Integer.parseInt(criteriaHolder.substring(0,1));
+            prefixCriteria = criteriaHolder.substring(1,criteriaHolder.length());
+            localRacePair = new Pair<>(prefixPoints,prefixCriteria);
+        } else {
+            localRacePair = new Pair<>(0,"");
+        }
+
+        if(!argMultimap.getAllValues(PREFIX_MEDICAL).isEmpty()){
+            criteriaHolder = argMultimap.getValue(PREFIX_MEDICAL).get();
+            prefixPoints = Integer.parseInt(criteriaHolder.substring(0,1));
+            prefixCriteria = criteriaHolder.substring(1,criteriaHolder.length());
+            localMedicalPair = new Pair<>(prefixPoints,prefixCriteria);
+        } else {
+            localMedicalPair = new Pair<>(0,"");
+        }
+
+        MapObject newMap = new MapObject(localAgePair, yearOperator, localRacePair, localMedicalPair);
+        return newMap;
     }
 
 }
