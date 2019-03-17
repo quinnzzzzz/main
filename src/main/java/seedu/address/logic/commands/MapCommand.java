319bc54b31473f85222f4c801c7f7fff9d9e13d3
articstranger <articstranger@hotmail.com>
2019-03-18 00:13:21 +0800

    MapCommand.java: updated usage description and added message success string

--- src/main/java/seedu/address/logic/commands/MapCommand.java
+++ src/main/java/seedu/address/logic/commands/MapCommand.java
@@ -8,6 +8,7 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.MapObject;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
@@ -23,17 +24,27 @@ public class MapCommand extends Command {
     public static final String COMMAND_WORD = "map";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose data fits any of "
-            + "the supplied criteria (case-insensitive) and assigns them points" +
-             "based on the order of criteria given, 3 for the first, 2 for the second, 1 for the last.\n"
-            + "Parameters: (Corresponding prefix and criteria 1) (Corresponding prefix and criteria 2) " +
-            "(Corresponding prefix and criteria 3)\n"
-            + "Example: map y/18> r/chinese";
+            + "the supplied criteria (case-insensitive, minimum 1, maximum 3) and assigns them points" +
+             "based on the number given.\n"
+            + "Parameters: (Corresponding prefix, points and criteria 1) (Corresponding prefix, points and criteria 2) " +
+            "(Corresponding prefix, points and criteria 3)\n"
+            + "Example: map y/3>18 r/2chinese";
+
+    public static final String MESSAGE_SUCCESS = "Mapping complete!";
+
+    private MapObject map;
+
+    public MapCommand(MapObject newMap) {
+        requireNonNull(newMap);
+        map = newMap;
+    }
+
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
         requireNonNull(model);
 
-        throw new CommandException("not ready");
+
 
     }
 
