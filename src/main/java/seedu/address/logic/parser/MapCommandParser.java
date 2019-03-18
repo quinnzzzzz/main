cce0fb1b640e6dbd6aacd3b3b32c1e9f41fba6c5
Quinn Zhao <amourmq@gmail.com>
2019-03-18 20:12:30 +0800

    V1.2 debug

--- src/main/java/seedu/address/logic/parser/MapCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MapCommandParser.java
@@ -1,12 +1,9 @@
 package seedu.address.logic.parser;
 
 import javafx.util.Pair;
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.MapCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.MapObject;
-
-import java.util.Arrays;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntaxProject.*;
