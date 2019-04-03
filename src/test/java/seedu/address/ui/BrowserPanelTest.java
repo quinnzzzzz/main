291703a554ccaae238914a0d5dbb1cfd4d962c5e
ndhuu <danghuu.dna@gmail.com>
2019-04-04 05:50:55 +0800

    fix checkstyle

--- src/test/java/seedu/address/ui/BrowserPanelTest.java
+++ src/test/java/seedu/address/ui/BrowserPanelTest.java
@@ -33,7 +33,8 @@ public class BrowserPanelTest extends GuiUnitTest {
 
         // associated web page of a person
         guiRobot.interact(() -> selectedVolunteer.set(ALICE));
-        URL expectedVolunteerUrl = new URL(BrowserPanel.SEARCH_PAGE_URL + ALICE.getName().fullName.replaceAll(" ", "%20"));
+        URL expectedVolunteerUrl = new URL(BrowserPanel.SEARCH_PAGE_URL
+            + ALICE.getName().fullName.replaceAll(" ", "%20"));
 
         waitUntilBrowserLoaded(browserPanelHandle);
         assertEquals(expectedVolunteerUrl, browserPanelHandle.getLoadedUrl());
