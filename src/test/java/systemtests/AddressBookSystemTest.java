b0748f57bc0f2a6d3fcf85bb161360fc4683dd83
ndhuu <danghuu.dna@gmail.com>
2019-04-04 05:50:55 +0800

    fix checkstyle

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -47,13 +47,14 @@ import seedu.address.ui.CommandBox;
 public abstract class AddressBookSystemTest {
     @ClassRule
     public static ClockRule clockRule = new ClockRule();
-    private MainWindowHandle mainWindowHandle;
-    private TestApp testApp;
-    private SystemTestSetupHelper setupHelper;
 
     private static final List<String> COMMAND_BOX_DEFAULT_STYLE = Arrays.asList("text-input", "text-field");
     private static final List<String> COMMAND_BOX_ERROR_STYLE =
         Arrays.asList("text-input", "text-field", CommandBox.ERROR_STYLE_CLASS);
+
+    private MainWindowHandle mainWindowHandle;
+    private TestApp testApp;
+    private SystemTestSetupHelper setupHelper;
 
     @BeforeClass
     public static void setupBeforeClass() {
