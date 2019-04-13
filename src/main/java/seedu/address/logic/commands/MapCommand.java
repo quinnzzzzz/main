51975209db6df1f7f62aba8124689c4cb72cf2fc
articstranger <articstranger@hotmail.com>
2019-04-13 21:08:32 +0800

    MapCommandTest fully implemented + miscellaneous changes

--- src/main/java/seedu/address/logic/commands/MapCommand.java
+++ src/main/java/seedu/address/logic/commands/MapCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.MapObject;
 import seedu.address.model.Model;
@@ -39,7 +38,7 @@ public class MapCommand extends Command {
         requireNonNull(model);
         model.mapAllVolunteer(map);
         return new CommandResult(
-            String.format(Messages.MESSAGE_MAP_SUCCESS));
+            String.format(MESSAGE_SUCCESS));
     }
 
 }
--- src/test/java/seedu/address/logic/commands/MapCommandTest.java
+++ src/test/java/seedu/address/logic/commands/MapCommandTest.java
@@ -1,4 +1,65 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.testutil.TypicalVolunteers.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalVolunteers.getTypicalVolunteersPoints;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import javafx.util.Pair;
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.MapObject;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
 public class MapCommandTest {
+
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void test_emptyAddressBook_success() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+        CommandResult result = new MapCommand(defaultMap()).execute(model, commandHistory);
+        assertEquals(model, expectedModel);
+    }
+
+    @Test
+    public void test_nonEmptyAddressBook_success() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        new MapCommand(defaultMap()).execute(model, commandHistory);
+        ArrayList<Integer> expectedPoints = getTypicalVolunteersPoints();
+        assertEquals(collectPoints(model), expectedPoints);
+    }
+
+    /**
+     *
+     * Goes through the @param model and collects points from all volunteers
+     * @return points as an arraylist
+     */
+    public ArrayList<Integer> collectPoints(Model model) {
+        ArrayList<Integer> points = new ArrayList<>();
+        model.getFilteredVolunteerList().forEach(vol -> {
+            points.add(vol.getPoints());
+        });
+        return points;
+    }
+
+    /**
+     * default mapobject stub
+     * @return
+     */
+    private MapObject defaultMap(){
+        Pair<Integer, Integer> agePair = new Pair<>(2, 18);
+        Pair<Integer, String> racePair = new Pair<>(1, "Chinese");
+        Pair<Integer, String> medicalPair = new Pair<>(3, "Nil");
+        String comparator = ">";
+
+        return new MapObject(agePair, comparator, racePair, medicalPair);
+    }
+
+
+
 }
