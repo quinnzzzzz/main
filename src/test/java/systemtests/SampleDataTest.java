e9092028cc81c0934b61e826881e1ac09089fc0b
Quinn Zhao <amourmq@gmail.com>
2019-04-15 23:28:20 +0800

    Update test files

--- src/test/java/systemtests/SampleDataTest.java
+++ src/test/java/systemtests/SampleDataTest.java
@@ -1,39 +1,39 @@
-//package systemtests;
-//
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//
-//import seedu.address.model.AddressBook;
-//import seedu.address.testutil.TestUtil;
-//
-//public class SampleDataTest extends AddressBookSystemTest {
-//    /**
-//     * Returns null to force test app to load data of the file in {@code getDataFileLocation()}.
-//     */
-//    @Override
-//    protected AddressBook getInitialData() {
-//        return null;
-//    }
-//
-//    /**
-//     * Returns a non-existent file location to force test app to load sample data.
-//     */
-//    @Override
-//    protected Path getDataFileLocation() {
-//        Path filePath = TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
-//        deleteFileIfExists(filePath);
-//        return filePath;
-//    }
-//
-//    /**
-//     * Deletes the file at {@code filePath} if it exists.
-//     */
-//    private void deleteFileIfExists(Path filePath) {
-//        try {
-//            Files.deleteIfExists(filePath);
-//        } catch (IOException ioe) {
-//            throw new AssertionError(ioe);
-//        }
-//    }
-//}
+package systemtests;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import seedu.address.model.AddressBook;
+import seedu.address.testutil.TestUtil;
+
+public class SampleDataTest extends AddressBookSystemTest {
+    /**
+     * Returns null to force test app to load data of the file in {@code getDataFileLocation()}.
+     */
+    @Override
+    protected AddressBook getInitialData() {
+        return null;
+    }
+
+    /**
+     * Returns a non-existent file location to force test app to load sample data.
+     */
+    @Override
+    protected Path getDataFileLocation() {
+        Path filePath = TestUtil.getFilePathInSandboxFolder("SomeFileThatDoesNotExist1234567890.xml");
+        deleteFileIfExists(filePath);
+        return filePath;
+    }
+
+    /**
+     * Deletes the file at {@code filePath} if it exists.
+     */
+    private void deleteFileIfExists(Path filePath) {
+        try {
+            Files.deleteIfExists(filePath);
+        } catch (IOException ioe) {
+            throw new AssertionError(ioe);
+        }
+    }
+}
